7004923242a59b448dfc584fccb0990718423160
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:42 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.ui.map): update MapViewModel.kt part 1

---
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/ui/map/MapViewModel.kt
@@ -0,0 +1,8 @@
+package com.bhumap.app.ui.map
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.bhumap.app.data.repository.PlotRepository
+import com.bhumap.app.domain.model.Plot
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
