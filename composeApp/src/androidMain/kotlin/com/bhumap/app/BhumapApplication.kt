fe224a54307a5e63d610af980ee167c583beef8f
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:08 +0530

    feat(composeApp.src.androidMain.kotlin.com.bhumap.app): update BhumapApplication.kt part 1

---
+++ composeApp/src/androidMain/kotlin/com/bhumap/app/BhumapApplication.kt
@@ -0,0 +1,8 @@
+package com.bhumap.app
+
+import android.app.Application
+import com.bhumap.app.di.appModule
+import com.bhumap.app.di.databaseModule
+import com.bhumap.app.di.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
