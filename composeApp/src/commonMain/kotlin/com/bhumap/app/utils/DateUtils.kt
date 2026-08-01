c68ca2bdb1b1c6b10428aecb0190c7a694497163
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:50 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.utils): update DateUtils.kt part 4

--- composeApp/src/commonMain/kotlin/com/bhumap/app/utils/DateUtils.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/utils/DateUtils.kt
@@ -7,3 +7,6 @@ import kotlinx.datetime.format.char
  * Basic Date formatting utilities using kotlinx-datetime.
  */
 fun String.toFormattedDate(): String {
+    return try {
+        val instant = Instant.parse(this)
+        val localDate = instant.toLocalDateTime(TimeZone.currentSystemDefault()).date
