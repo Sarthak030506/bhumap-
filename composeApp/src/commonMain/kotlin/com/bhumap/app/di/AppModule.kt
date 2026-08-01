199848303e2c5e86436738ea27b33da4797a8613
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:24 +0530

    feat/build: add Transaction$$serializer.dex

--- composeApp/src/commonMain/kotlin/com/bhumap/app/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/di/AppModule.kt
@@ -5,3 +5,10 @@ import com.bhumap.app.data.repository.CustomerRepository
 import com.bhumap.app.data.repository.LandRepository
 import com.bhumap.app.data.repository.PlotRepository
 import com.bhumap.app.ui.auth.AuthViewModel
+import com.bhumap.app.ui.customers.CustomerViewModel
+import com.bhumap.app.ui.dashboard.DashboardViewModel
+import com.bhumap.app.ui.land.LandViewModel
+import com.bhumap.app.ui.map.MapViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
