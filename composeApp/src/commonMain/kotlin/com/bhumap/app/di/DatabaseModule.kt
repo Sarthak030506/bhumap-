9f974c2900ece0baf9e9b4fc8548c62b32b1fb92
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:25 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.di): update DatabaseModule.kt part 2

--- composeApp/src/commonMain/kotlin/com/bhumap/app/di/DatabaseModule.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/di/DatabaseModule.kt
@@ -1,3 +1,6 @@
 package com.bhumap.app.di
 
 import com.bhumap.app.data.local.DatabaseDriverFactory
+import com.bhumap.app.data.local.db.BhumapDatabase
+import org.koin.core.scope.Scope
+import org.koin.dsl.module
