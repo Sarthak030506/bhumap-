7d01c2db0f95925eb5aee55d3fe0aa48975f1b75
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:10 +0530

    feat(composeApp.src.iosMain.kotlin.com.bhumap.app.ui.map): update PlatformMapView.kt part 1

---
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/ui/map/PlatformMapView.kt
@@ -0,0 +1,8 @@
+package com.bhumap.app.ui.map
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
