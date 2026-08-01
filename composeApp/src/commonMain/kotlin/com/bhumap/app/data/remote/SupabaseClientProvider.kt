5762c348b46b63e200a090d6de45a5d9a9baa1b8
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:18 +0530

    feat/build: add NetworkModuleKt.dex

--- composeApp/src/commonMain/kotlin/com/bhumap/app/data/remote/SupabaseClientProvider.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/data/remote/SupabaseClientProvider.kt
@@ -4,3 +4,9 @@ import io.github.jan.supabase.auth.Auth
 import io.github.jan.supabase.createSupabaseClient
 import io.github.jan.supabase.postgrest.Postgrest
 import io.github.jan.supabase.storage.Storage
+
+/**
+ * Supabase client singleton.
+ * URL and anon key are injected by Koin from BuildConfig / environment.
+ */
+fun createSupabase(url: String, anonKey: String) = createSupabaseClient(
