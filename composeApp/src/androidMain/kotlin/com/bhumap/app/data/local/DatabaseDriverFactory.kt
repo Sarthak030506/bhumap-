d00521c5ebe7526e90f1b49e85791c638e28eab4
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:10 +0530

    feat/build: add TransactionQueries$SelectBySaleQuery.dex

--- composeApp/src/androidMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
+++ composeApp/src/androidMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
@@ -2,3 +2,7 @@ package com.bhumap.app.data.local
 
 import android.content.Context
 import app.cash.sqldelight.driver.android.AndroidSqliteDriver
+import app.cash.sqldelight.db.SqlDriver
+import com.bhumap.app.data.local.db.BhumapDatabase
+
+actual class DatabaseDriverFactory actual constructor(context: Any?) {
