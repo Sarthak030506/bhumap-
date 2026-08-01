e9e31ed212fe3624e130503ef67162641c083e2d
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:08 +0530

    feat(composeApp.src.iosMain.kotlin.com.bhumap.app.data.local): update DatabaseDriverFactory.kt part 7

--- composeApp/src/iosMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
@@ -4,3 +4,7 @@ import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
 import com.bhumap.app.data.local.db.BhumapDatabase
 
+actual class DatabaseDriverFactory actual constructor(context: Any?) {
+    actual fun createDriver(): SqlDriver =
+        NativeSqliteDriver(BhumapDatabase.Schema, "bhumap.db")
+}
