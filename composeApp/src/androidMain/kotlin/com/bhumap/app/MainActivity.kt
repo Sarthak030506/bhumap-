38ebe53e091330ecfd32a3e58245a207ba3a0a32
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:09 +0530

    feat(composeApp.src.androidMain.kotlin.com.bhumap.app): update MainActivity.kt part 1

---
+++ composeApp/src/androidMain/kotlin/com/bhumap/app/MainActivity.kt
@@ -0,0 +1,8 @@
+package com.bhumap.app
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
