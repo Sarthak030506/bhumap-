5bc88d2a499dbe440f06aef4658abdda963b4178
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:07 +0530

    feat(composeApp.src.iosMain.kotlin.com.bhumap.app.data.local): update DatabaseDriverFactory.kt part 5

--- composeApp/src/iosMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
@@ -2,3 +2,4 @@ package com.bhumap.app.data.local
 
 import app.cash.sqldelight.db.SqlDriver
 import app.cash.sqldelight.driver.native.NativeSqliteDriver
+import com.bhumap.app.data.local.db.BhumapDatabase
