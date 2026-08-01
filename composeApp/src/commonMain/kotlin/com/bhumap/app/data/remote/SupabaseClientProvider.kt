4329c9dbd0941d96b36b89b8b41be243b7a61153
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:18 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.data.remote): update SupabaseClientProvider.kt part 1

---
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/data/remote/SupabaseClientProvider.kt
@@ -0,0 +1,6 @@
+package com.bhumap.app.data.remote
+
+import io.github.jan.supabase.auth.Auth
+import io.github.jan.supabase.createSupabaseClient
+import io.github.jan.supabase.postgrest.Postgrest
+import io.github.jan.supabase.storage.Storage
