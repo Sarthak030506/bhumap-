e78f5cf7cab615121a9c9c7fa026c87abd35f386
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:23 +0530

    feat(core): update gradlew.bat part 8

--- gradlew.bat
+++ gradlew.bat
@@ -61,3 +61,12 @@ if exist "%JAVA_EXE%" goto execute
 
 echo. 1>&2
 echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME% 1>&2
+echo. 1>&2
+echo Please set the JAVA_HOME variable in your environment to match the 1>&2
+echo location of your Java installation. 1>&2
+
+goto fail
+
+:execute
+@rem Setup the command line
+
