9183bc07a08a19246ad3b1efb990fa16db5dcb50
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:05 +0530

    feat/build: add class-fq-name-to-source.tab_i.len

--- composeApp/src/iosMain/kotlin/com/bhumap/app/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/MainViewController.kt
@@ -1,3 +1,6 @@
 package com.bhumap.app
 
 import androidx.compose.ui.window.ComposeUIViewController
+import com.bhumap.app.di.appModule
+import com.bhumap.app.di.databaseModule
+import com.bhumap.app.di.networkModule
