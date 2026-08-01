5199ef6642da2c892241e88361d595821e50e465
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:26 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.di): update NetworkModule.kt part 4

--- composeApp/src/commonMain/kotlin/com/bhumap/app/di/NetworkModule.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/di/NetworkModule.kt
@@ -4,3 +4,6 @@ import com.bhumap.app.BuildConfig
 import com.bhumap.app.data.remote.createSupabase
 import org.koin.dsl.module
 
+val networkModule = module {
+    single { createSupabase(BuildConfig.SUPABASE_URL, BuildConfig.SUPABASE_ANON_KEY) }
+}
