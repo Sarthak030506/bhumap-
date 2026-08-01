0af672169c959a2dfdc1a6fffe0433d13339c85c
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:34:40 +0530

    feat/build: add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.androidApplication) apply false
+    alias(libs.plugins.androidLibrary) apply false
+    alias(libs.plugins.jetbrainsCompose) apply false
+    alias(libs.plugins.compose.compiler) apply false
+    alias(libs.plugins.kotlinMultiplatform) apply false
+    alias(libs.plugins.kotlinSerialization) apply false
+    alias(libs.plugins.sqldelight) apply false
+    alias(libs.plugins.googleServices) apply false
+    alias(libs.plugins.buildconfig) apply false
+}
