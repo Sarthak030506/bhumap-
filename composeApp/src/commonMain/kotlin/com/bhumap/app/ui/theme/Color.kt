f7b6097aeb29b58b62198d4efcc40b1b01d105af
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:46 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.ui.theme): update Color.kt part 2

--- composeApp/src/commonMain/kotlin/com/bhumap/app/ui/theme/Color.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/ui/theme/Color.kt
@@ -9,3 +9,14 @@ val Evergreen700  = Color(0xFF1A6147)
 val Evergreen     = Color(0xFF1F6F50)  // Primary
 val Evergreen400  = Color(0xFF3D8F6D)
 val Evergreen200  = Color(0xFF93C9B2)
+val Evergreen50   = Color(0xFFE8F5EF)
+
+val Terracotta    = Color(0xFFC8552B)  // Accent / Error
+val Terracotta200 = Color(0xFFEDAF9A)
+val Terracotta50  = Color(0xFFFAEDE8)
+
+val Paper50       = Color(0xFFFBF7F0)  // Surface / Background
+val Paper100      = Color(0xFFF3EDE3)
+val Paper200      = Color(0xFFE8DDD0)
+
+val Soil900       = Color(0xFF2A1F14)  // On-surface text
