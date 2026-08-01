db5b4aa283b686e9e021424c4b5bfda4560a8b2b
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:45 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.ui.navigation): update Screen.kt part 1

---
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/ui/navigation/Screen.kt
@@ -0,0 +1,9 @@
+package com.bhumap.app.ui.navigation
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Dashboard
+import androidx.compose.material.icons.filled.Landscape
+import androidx.compose.material.icons.filled.Map
+import androidx.compose.material.icons.filled.People
+import androidx.compose.ui.graphics.vector.ImageVector
+
