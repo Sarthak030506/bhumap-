99ea9e5067406782175fb7fe0521eb035c48e922
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:30 +0530

    feat/build: add ComposableSingletons$AppKt.class

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,3 +8,15 @@ pluginManagement {
         gradlePluginPortal()
         mavenCentral()
     }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
+    }
+}
+
+include(":composeApp")
