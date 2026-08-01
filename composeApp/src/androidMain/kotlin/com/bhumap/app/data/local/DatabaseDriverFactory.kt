f22cabb072cad68e0ada11669237341ef1dec63c
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:10 +0530

    feat/build: add TransactionQueries$SumBySaleQuery.dex

--- composeApp/src/androidMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
+++ composeApp/src/androidMain/kotlin/com/bhumap/app/data/local/DatabaseDriverFactory.kt
@@ -6,3 +6,7 @@ import app.cash.sqldelight.db.SqlDriver
 import com.bhumap.app.data.local.db.BhumapDatabase
 
 actual class DatabaseDriverFactory actual constructor(context: Any?) {
+    private val androidContext = context as Context
+    actual fun createDriver(): SqlDriver =
+        AndroidSqliteDriver(BhumapDatabase.Schema, androidContext, "bhumap.db")
+}
