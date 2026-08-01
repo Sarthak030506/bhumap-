c7f6b2ada8b4df6fc5eec51ef7d890510469df2e
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:47 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.ui.theme): update Theme.kt part 1

---
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/ui/theme/Theme.kt
@@ -0,0 +1,8 @@
+package com.bhumap.app.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.lightColorScheme
+import androidx.compose.runtime.Composable
+
+private val LightColorScheme = lightColorScheme(
+    primary             = md_theme_light_primary,
