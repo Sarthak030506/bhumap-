f1b87e784ad4595d6e50c3ec58c85e5c1e7d5d0e
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:09 +0530

    feat/build: add internal-name-to-source.tab.values.at

--- composeApp/src/iosMain/kotlin/com/bhumap/app/di/DatabaseModule.ios.kt
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/di/DatabaseModule.ios.kt
@@ -2,3 +2,5 @@ package com.bhumap.app.di
 
 import com.bhumap.app.data.local.DatabaseDriverFactory
 import org.koin.core.scope.Scope
+
+actual fun Scope.createDatabaseDriverFactory(): DatabaseDriverFactory {
