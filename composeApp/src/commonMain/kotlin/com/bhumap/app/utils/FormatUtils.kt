bbac4a742b9d51cc53a260e5c65d3246095bbc6b
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:52 +0530

    feat(composeApp.src.commonMain.kotlin.com.bhumap.app.utils): update FormatUtils.kt part 3

--- composeApp/src/commonMain/kotlin/com/bhumap/app/utils/FormatUtils.kt
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/utils/FormatUtils.kt
@@ -10,3 +10,9 @@ fun Double.formatINR(): String {
     val parts = "%.2f".format(abs).split(".")
     val intPart = parts[0]
     val decPart = parts[1]
+
+    val result = when {
+        intPart.length <= 3 -> intPart
+        else -> {
+            val last3 = intPart.takeLast(3)
+            val rest  = intPart.dropLast(3)
