15642c63881dcacb9285666070950f6a77d27e3e
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:37:05 +0530

    feat/build: add complementary-files.tab

--- composeApp/src/iosMain/kotlin/com/bhumap/app/MainViewController.kt
+++ composeApp/src/iosMain/kotlin/com/bhumap/app/MainViewController.kt
@@ -4,3 +4,6 @@ import androidx.compose.ui.window.ComposeUIViewController
 import com.bhumap.app.di.appModule
 import com.bhumap.app.di.databaseModule
 import com.bhumap.app.di.networkModule
+import org.koin.core.context.startKoin
+
+/**
