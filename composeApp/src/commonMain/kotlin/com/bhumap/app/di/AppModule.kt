4f83fa4af25ca5cc1da2eada0845e4b3072b6d0f
Sarthak030506 <Sarthak030506@users.noreply.github.com>
2026-08-01 12:36:24 +0530

    feat/build: add SaleStatus.dex

---
+++ composeApp/src/commonMain/kotlin/com/bhumap/app/di/AppModule.kt
@@ -0,0 +1,7 @@
+package com.bhumap.app.di
+
+import com.bhumap.app.data.repository.AuthRepository
+import com.bhumap.app.data.repository.CustomerRepository
+import com.bhumap.app.data.repository.LandRepository
+import com.bhumap.app.data.repository.PlotRepository
+import com.bhumap.app.ui.auth.AuthViewModel
